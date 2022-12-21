75a42babfa1680879ef0badf761750c90eeab6b7
harveybland <harvey.bland@genius.online>
2022-12-21 15:36:03 +0000

    updates

--- src/app/main/event/event.component.ts
+++ src/app/main/event/event.component.ts
@@ -7,42 +7,48 @@ import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-event',
   templateUrl: './event.component.html',
-  styleUrls: ['./event.component.scss']
+  styleUrls: ['./event.component.scss'],
 })
 export class EventComponent implements OnInit, OnDestroy {
-
   userId: any;
   eventId: any;
   event: any;
 
-  constructor(private _activatedRoute: ActivatedRoute,
+  constructor(
+    private _activatedRoute: ActivatedRoute,
     private _mainService: MainService,
-    private _jwtService: JwtStorageService,
-  ) { }
+    private _jwtService: JwtStorageService
+  ) {}
 
   ngOnInit() {
     let Id = this._jwtService.getUserId();
     this.userId = Id;
-    this._activatedRoute.params.pipe(
-      map((params: any) => {
-        return params['id'] as string;
-      }),
-      switchMap((id: any) => {
-        this.eventId = id;
-        return this._mainService.getEvent(id).pipe(tap((model: any) => {
-          this.event = model;
-          let updateViews = {
-            viewed: this.event.viewed + 1
-          }
-          if (model.userId != this.userId) {
-            this._mainService.updateViews(this.eventId, updateViews).subscribe();
-          }
-        }))
-      })).subscribe();
+    this._activatedRoute.params
+      .pipe(
+        map((params: any) => {
+          return params['id'] as string;
+        }),
+        switchMap((id: any) => {
+          this.eventId = id;
+          return this._mainService.getEvent(id).pipe(
+            tap((model: any) => {
+              this.event = model;
+              let updateViews = {
+                viewed: this.event.viewed + 1,
+              };
+              if (model.userId != this.userId) {
+                this._mainService
+                  .updateViews(this.eventId, updateViews)
+                  .subscribe();
+              }
+            })
+          );
+        })
+      )
+      .subscribe();
   }
 
   ngOnDestroy() {
     this.userId = '';
   }
-
 }
