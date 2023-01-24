170de08ddc325bb92195987ce975bb3625cfc370
harveybland <harvey.bland@genius.online>
2023-01-24 10:07:22 +0000

    updates

--- src/app/user/sign-up/sign-up.component.ts
+++ src/app/user/sign-up/sign-up.component.ts
@@ -10,12 +10,18 @@ import { Router } from '@angular/router';
   styleUrls: ['./sign-up.component.scss'],
 })
 export class SignUpComponent implements OnInit {
-  email = new FormControl('', [Validators.required, Validators.email]);
-  password = new FormControl('', [Validators.required]);
+  personal = false;
+  submitted = false;
 
   signUpForm: FormGroup = new FormGroup({
-    email: this.email,
-    password: this.password,
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required]),
+    businessName: new FormControl('', [Validators.required]),
+    fullName: new FormControl('', [Validators.required]),
+    number: new FormControl('', [Validators.required]),
+    addressLine1: new FormControl('', [Validators.required]),
+    addressLine2: new FormControl('', [Validators.required]),
+    town: new FormControl('', [Validators.required]),
   });
 
   showSucessMessage: boolean;
@@ -34,35 +40,57 @@ export class SignUpComponent implements OnInit {
   }
 
   onSubmit() {
-    let model = this.userModel();
-    this._userService.create(model).subscribe(
-      (res) => {
-        return this._userService.login(model).subscribe((res) => {
-          this._jwtService.setToken(res['token']);
-          this._router.navigateByUrl('/ui/profile');
-        });
-      },
-      (err) => {
-        if (err.status === 422) {
-          this.serverErrorMessages = err.error.join('<br/>');
-        } else
-          this.serverErrorMessages =
-            'Something went wrong.Please contact admin.';
-      }
-    );
+    this.submitted = true;
+    if (this.signUpForm.invalid) {
+      Object.keys(this.signUpForm.controls).forEach((key) => {
+        this.signUpForm.get(key)!.hasError('required');
+      });
+    } else {
+      let model = this.userModel();
+      this._userService.create(model).subscribe(
+        (res) => {
+          return this._userService.login(model).subscribe((res) => {
+            this.submitted = false;
+            this._jwtService.setToken(res['token']);
+            this._router.navigateByUrl('/ui/profile');
+          });
+        },
+        (err) => {
+          if (err.status === 422) {
+            this.serverErrorMessages = err.error.join('<br/>');
+          } else
+            this.serverErrorMessages =
+              'Something went wrong.Please contact admin.';
+        }
+      );
+    }
   }
 
   userModel() {
-    return {
-      email: this.signUpForm.controls.email.value,
-      password: this.signUpForm.controls.password.value,
-      businessName: null,
-      fullName: null,
-      number: null,
-      addressLine1: null,
-      addressLine2: null,
-      town: null,
-      profileComplete: false,
-    };
+    if (this.personal) {
+      return {
+        email: this.signUpForm.controls.email.value,
+        password: this.signUpForm.controls.password.value,
+        businessName: null,
+        fullName: this.signUpForm.controls.fullName.value,
+        number: this.signUpForm.controls.number.value,
+        addressLine1: this.signUpForm.controls.addressLine1.value,
+        addressLine2: this.signUpForm.controls.addressLine2.value,
+        town: this.signUpForm.controls.town.value,
+        profileComplete: false,
+      };
+    } else {
+      return {
+        email: this.signUpForm.controls.email.value,
+        password: this.signUpForm.controls.password.value,
+        businessName: this.signUpForm.controls.businessName.value,
+        fullName: null,
+        number: this.signUpForm.controls.number.value,
+        addressLine1: this.signUpForm.controls.addressLine1.value,
+        addressLine2: this.signUpForm.controls.addressLine2.value,
+        town: this.signUpForm.controls.town.value,
+        profileComplete: false,
+      };
+    }
   }
 }
